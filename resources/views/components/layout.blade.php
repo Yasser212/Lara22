52aa1a35af21162fc4ea8d5787ce21540849ffbd
Yasser212 <yasseralsayed@gmx.de>
2022-05-02 23:56:32 +0200

    Change some tw classes in main section

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
 </head>
 
 <body class="mb-48">
-    <nav class="flex justify-between items-center mb-4">
+    <nav class="flex justify-between items-center">
         <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt=""
                 class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
